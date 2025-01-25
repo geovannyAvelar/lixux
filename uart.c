177dd295b4214498948d11623d6174c07f369639
Giovanni Avelar <geovanny.carneiro@iacit.com.br>
2025-01-24 21:14:17 -0300

    Adjusting types on UART driver

--- uart.c
+++ uart.c
@@ -15,38 +15,41 @@
 #define IER_RX_ENABLE (1<<0)
 
 void uart_init() {
-  volatile uint8_t* uart_ier = (volatile uint8_t*) UART_ADDRESS + IER;
+  volatile uint8_t* uart_base = (volatile uint8_t*) UART_ADDRESS;
+
+  volatile uint8_t* uart_ier = (volatile uint8_t*) uart_base + IER;
   *uart_ier = 0x0;
 
-  volatile uint8_t* uart_lcr = (volatile uint8_t*) UART_ADDRESS + LCR;
+  volatile uint8_t* uart_lcr = (volatile uint8_t*) uart_base + LCR;
   *uart_lcr = LCR_BAUD_LATCH;
 
-  volatile uint8_t* uart = (volatile uint8_t*) UART_ADDRESS;
+  volatile uint8_t* uart = (volatile uint8_t*) uart_base;
   *uart = 0x03;
 
   *uart_lcr = (volatile uint8_t*) LCR_EIGHT_BITS;
 
-  volatile uint8_t* uart_fcr = (volatile uint8_t*) UART_ADDRESS + FCR;
+  volatile uint8_t* uart_fcr = (volatile uint8_t*) uart_base + FCR;
   *uart_fcr = FCR_FIFO_ENABLE | FCR_FIFO_CLEAR;
 
   *uart_ier = IER_TX_ENABLE | IER_RX_ENABLE;
 }
 
 char uart_getc() {
-  uint8_t uart_lsr = UART_ADDRESS + LSR;
-  uint8_t uart_line_status_data_ready = UART_ADDRESS + 1;
+  volatile uint8_t* uart_base = (volatile uint8_t*) UART_ADDRESS;
+  volatile uint8_t* uart_lsr = (volatile uint8_t*) uart_base + LSR;
+  volatile uint8_t* uart_line_status_data_ready = (volatile uint8_t*) uart_base + 1;
 
-  if (uart_lsr & uart_line_status_data_ready == 0) {
+  if ((*uart_lsr & *uart_line_status_data_ready) == 0) {
     return 0;
   }
 
-  char* c = UART_ADDRESS;
+  char* c = uart_base;
 
   return *c;
 }
 
 void uart_putc(const char c) {
-  char* uart = UART_ADDRESS;
+  volatile char* uart = (volatile char*) UART_ADDRESS;
   *uart = c;
 }
 
