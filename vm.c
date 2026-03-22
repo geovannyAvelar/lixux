31dad991528249f90edac7e62135ca0d1cb9a9ab
Giovanni Avelar <geovanny.avelar@gmail.com>
2026-03-22 14:56:01 -0300

    Add basic physical memory allocator and initialization

---
+++ vm.c
@@ -0,0 +1,30 @@
+#include "vm.h"
+#include "types.h"
+
+static struct run *freelist;
+
+void mem_init(void)
+{
+    char *p = (char*) PGROUNDUP((uint64_t)_end);
+    for (; p + PGSIZE <= (char*)PHYSTOP; p += PGSIZE)
+        kfree(p);
+}
+
+void freerange(void *start, void *end) {
+    char *p;
+    for (p = (char*)start; p + PGSIZE <= (char*)end; p += PGSIZE)
+        kfree(p);
+}
+
+void *kalloc(void) {
+    struct run *r = freelist;
+    if (r)
+        freelist = r->next;
+    return (void*)r;
+}
+
+void kfree(void *p) {
+    struct run *r = (struct run*)p;
+    r->next = freelist;
+    freelist = r;
+}
